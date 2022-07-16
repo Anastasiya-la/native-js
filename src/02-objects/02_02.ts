5d4679a9fc3e6b1f29b6478ad6e85dd7e01c7a0a
Anastasiya Rabetskaya <nastia1362@gmail.com>
2022-07-16 17:20:47 +0300

    objects

---
+++ src/02-objects/02_02.ts
@@ -0,0 +1,27 @@
+export type CityType = {
+    title: string
+    houses: HouseType[]
+    governmentBuildings: Array<GovernmentBuildingType>
+    citizensNumber: number
+}
+
+export type AddressType = {
+    number?: number
+    street: StreetType
+}
+type StreetType = {
+    title: string
+}
+
+type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+
+type GovernmentBuildingType = {
+    type: 'HOSPITAL' | 'FIRE_STATION'
+    budget: number
+    staffCount: number
+    address: AddressType
+}
